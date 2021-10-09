a46c557397defab8868ca336c6bce1b3677b8e8f
KirillSpecial <89597568+KirillSpecial@users.noreply.github.com>
2021-10-09 22:17:17 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,5 @@
+function myScript(){
+    let firstName = prompt('Как Вас зовут?');
+    let lastName = prompt('Какая ваша фамилия?');
+    alert("Добро пожаловать " + firstName + " " + lastName);
+ }
